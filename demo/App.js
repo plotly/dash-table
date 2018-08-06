bba38bd22886624daab25a7c9cbf89a92a5e0d99
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2018-08-06 14:59:35 -0400

    fix demo code...

---
+++ demo/App.js
@@ -0,0 +1,95 @@
+/* eslint no-magic-numbers: 0 */
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {Table} from 'dash-table';
+import {mockData} from './data';
+import Dropdown from 'react-select';
+import TestFixtures from 'tests/fixtures.json';
+import {merge} from 'ramda';
+import { memoizeOne } from 'core/memoizer';
+
+const clone = o => JSON.parse(JSON.stringify(o));
+
+class App extends Component {
+    constructor() {
+        super();
+
+        this.onChange = this.onChange.bind(this);
+
+        this.state = {
+            tableProps: {
+                id: 'table',
+                dataframe: clone(mockData.dataframe),
+                columns: clone(mockData.columns),
+                editable: true,
+                row_selectable: 'multi',
+                selected_rows: [5, 10, 15],
+            },
+            selectedFixture: null,
+        };
+
+        const getOptions = memoizeOne(fixtures => {
+            return fixtures.map(t => ({
+                label: t.name,
+                value: JSON.stringify(t.props),
+            }));
+        });
+
+        const setProps = memoizeOne(() => {
+            return newProps => {
+                console.info('--->', newProps);
+                this.setState({
+                    tableProps: merge(this.state.tableProps, newProps),
+                });
+            };
+        });
+
+        Object.defineProperty(this, 'options', {
+            get: () => getOptions(TestFixtures)
+        });
+
+        Object.defineProperty(this, 'setProps', {
+            get: () => setProps()
+        });
+    }
+
+    onChange(e) {
+        this.setState({
+            tableProps: JSON.parse(e.value),
+            selectedFixture: e.value,
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {<div>
+                    <label>Load test case</label>
+                    <Dropdown
+                        options={this.options}
+                        onChange={this.onChange}
+                        value={this.state.selectedFixture}
+                    />
+                </div>}
+
+                <hr />
+                <label>test events:{'\u00A0\u00A0'}</label>
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+                <hr />
+
+                <Table
+                    setProps={this.setProps}
+                    {...this.state.tableProps}
+                />
+            </div>
+        );
+    }
+}
+
+App.propTypes = {
+    value: PropTypes.any,
+};
+
+export default App;
