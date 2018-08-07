eeb986067d8cab4cc693d1cb8653f9992abeca24
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2018-08-07 14:17:43 -0400

    fix lint

--- src/dash-table/components/ControlledTable.js
+++ src/dash-table/components/ControlledTable.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import * as R from 'ramda';
 import SheetClip from 'sheetclip';
 import { colIsEditable } from 'dash-table/components/derivedState';
