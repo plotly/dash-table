01327b5cc76ed1e03b11e276c9ec040c6e2d5c25
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2018-09-14 10:36:27 -0400

    refactor syntax tree wrapper

--- src/core/syntax-tree/index.ts
+++ src/core/syntax-tree/index.ts
@@ -1,31 +1,34 @@
 import Logger from 'core/Logger';
 import lexer from 'core/syntax-tree/lexer';
-import syntaxer, { ISyntaxTree } from 'core/syntax-tree/syntaxer';
+import syntaxer, { ISyntaxerResult } from 'core/syntax-tree/syntaxer';
 
 export default class SyntaxTree {
-    private syntaxTree?: ISyntaxTree;
+    private result: ISyntaxerResult;
 
     get isValid() {
-        return !!this.syntaxTree;
+        return this.result.valid;
+    }
+
+    private get tree() {
+        return this.result.tree;
     }
 
     constructor(private readonly query: string) {
-        const result = syntaxer(lexer(this.query));
-        this.syntaxTree = result.tree;
+        this.result = syntaxer(lexer(this.query));
     }
 
     evaluate = (target: any) => {
-        if (!this.syntaxTree) {
+        if (!this.isValid || !this.tree) {
             const msg = `unable to evaluate target: syntax tree is invalid for query=${this.query}`;
 
             Logger.error(msg);
             throw new Error(msg);
         }
 
-        const evaluate = this.syntaxTree.lexeme.evaluate;
+        const evaluate = this.tree.lexeme.evaluate;
 
         return evaluate ?
-            evaluate(target, this.syntaxTree) :
+            evaluate(target, this.tree) :
             false;
     }
 
