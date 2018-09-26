d96fc006b5baa09909f0938eee1b7a56fc616beb
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2018-09-26 10:19:49 -0400

    filter typing

--- src/dash-table/components/FilterFactory.tsx
+++ src/dash-table/components/FilterFactory.tsx
@@ -24,7 +24,7 @@ export interface IFilterOptions {
 
 export default class FilterFactory {
     private readonly handlers = new Map();
-    private readonly ops = new Map<ColumnId, string>();
+    private readonly ops = new Map<string, string>();
 
     private get props() {
         return this.propsFn();
@@ -142,13 +142,13 @@ export default class FilterFactory {
     }
 
     private isFragmentValidOrNull(columnId: ColumnId) {
-        const op = this.ops.get(columnId);
+        const op = this.ops.get(columnId.toString());
 
         return !op || !op.trim().length || this.isFragmentValid(columnId);
     }
 
     private isFragmentValid(columnId: ColumnId) {
-        const op = this.ops.get(columnId);
+        const op = this.ops.get(columnId.toString());
 
         const lexerResult = lexer(`${columnId} ${op}`);
         const syntaxerResult = syntaxer(lexerResult);
@@ -182,7 +182,7 @@ export default class FilterFactory {
                     isValid={this.isFragmentValidOrNull(column.id)}
                     property={column.id}
                     setFilter={this.getEventHandler(this.onChange, column.id, this.ops, setFilter)}
-                    value={this.ops.get(column.id)}
+                    value={this.ops.get(column.id.toString())}
                 />);
             }, columns) :
             [(<AdvancedFilter
