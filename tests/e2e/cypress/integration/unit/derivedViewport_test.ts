2494bced418a76043464f2bb56192c2b25e25dfb
Marc-André Rivet <Marc-Andre-Rivet@users.noreply.github.com>
2018-09-26 10:24:47 -0400

    Update derivedViewport_test.ts
    Add basic viewport test with |df| > page_size

--- tests/e2e/cypress/integration/unit/derivedViewport_test.ts
+++ tests/e2e/cypress/integration/unit/derivedViewport_test.ts
@@ -48,4 +48,24 @@ describe.only('derived viewport', () => {
             });
         });
     });
+    
+    describe('virtual dataframe > page size', () => {
+        describe('with fe pagination', () => {
+            it('returns slice of dataframe', () => {
+                const result = viewportDataframe(
+                    'fe',
+                    { displayed_pages: 1, current_page: 0, page_size: 250 },
+                    R.map(idx => ({ idx }), R.range(0, 500)),
+                    R.range(0, 500)
+                );
+
+                expect(result.dataframe.length).to.equal(250);
+                expect(result.indices.length).to.equal(250);
+                expect(result.dataframe[0].idx).to.equal(0);
+                expect(result.dataframe[249].idx).to.equal(249);
+                expect(result.indices[0]).to.equal(0);
+                expect(result.indices[249]).to.equal(249);
+            });
+        });
+    });
 });
