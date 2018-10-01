6a414c8f84ce11d12e9184ed76075df3438dbb95
Chris P <chris@plot.ly>
2018-10-01 16:34:53 -0400

    :rainbow: styling examples
    examples that represent of the level of customization that we need in
    dash-table. The examples are implemented with HTML tables to
    demonstrate the intended behaviour.
    
    I’d like to see each of these examples implemented with the
    `dash_table` syntax. We’ll use these examples as our `dash-table`
    documentation

--- tests/dash/utils.py
+++ tests/dash/utils.py
@@ -20,6 +20,12 @@ def html_table(
     column_style={},
     cell_style={},
     cell_style_by_column={},
+    header_style={},
+    header_style_by_column={},
+    row_style={},
+    row_style_by_index={},
+    odd_row_style={},
+    conditional_cell_style=(lambda cell, column: {})
 ):
     header = []
     for column in df.columns:
@@ -27,10 +33,12 @@ def html_table(
             html.Th(
                 column,
                 style=merge(
+                    row_style,
                     base_column_style,
                     cell_style,
                     column_style.get(column, {}),
-                    cell_style_by_column.get(column, {}),
+                    header_style,
+                    header_style_by_column.get(column, {}),
                 ),
             )
         )
@@ -42,7 +50,14 @@ def html_table(
             row.append(
                 html.Td(
                     df.iloc[i][column],
-                    style=merge(cell_style, cell_style_by_column.get(column, {})),
+                    style=merge(
+                        row_style,
+                        row_style_by_index.get(i, {}),
+                        odd_row_style if i % 2 == 1 else {},
+                        cell_style,
+                        cell_style_by_column.get(column, {}),
+                        conditional_cell_style(df.iloc[i][column], column)
+                    ),
                 )
             )
         rows.append(html.Tr(row))
