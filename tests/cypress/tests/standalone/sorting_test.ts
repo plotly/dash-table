e8b675112c144b0f169dd03ffe1b1d6a88ac8ee0
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2018-10-02 13:32:49 -0400

    fix regressions

--- tests/cypress/tests/standalone/sorting_test.ts
+++ tests/cypress/tests/standalone/sorting_test.ts
@@ -10,7 +10,7 @@ describe('sort', () => {
         DashTable.getCell(1, 6).within(() => cy.get('.Select-value-label').should('have.html', 'Snowy'));
         DashTable.getCell(2, 6).within(() => cy.get('.Select-value-label').should('have.html', 'Tropical Beaches'));
         DashTable.getCell(3, 6).within(() => cy.get('.Select-value-label').should('have.html', 'Humid'));
-        cy.get('tr th.column-6 .sort').click();
+        cy.get('tr th.column-6 .sort').last().click();
         DashTable.getCell(0, 6).within(() => cy.get('.Select-value-label').should('have.html', 'Wet'));
         DashTable.getCell(1, 6).within(() => cy.get('.Select-value-label').should('have.html', 'Wet'));
         DashTable.getCell(2, 6).within(() => cy.get('.Select-value-label').should('have.html', 'Wet'));
