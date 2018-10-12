bc8400fb2a83fee0cf19d54f29d64d03191c96f5
Chris P <chris@plot.ly>
2018-10-12 19:58:43 -0400

    :alarm_clock: increase the timer before snapshotting

--- tests/dash/test_integration.py
+++ tests/dash/test_integration.py
@@ -21,7 +21,7 @@ class Tests(IntegrationTests):
             WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.ID, "waitfor"))
             )
-            time.sleep(10)
+            time.sleep(30)
             self.snapshot(href)
             self.driver.back()
 
