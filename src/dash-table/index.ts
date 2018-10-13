2d3c9984ef74c802d81849c8ea1dd436f873fe98
Chris P <chris@plot.ly>
2018-10-12 21:30:38 -0400

    :package: new build

--- src/dash-table/index.ts
+++ src/dash-table/index.ts
@@ -2,10 +2,12 @@ import Environment from 'core/environment';
 import Logger from 'core/Logger';
 
 import Table from 'dash-table/Table';
+import TableLite from 'dash-table/TableLite';
 
 Logger.setDebugLevel(Environment.debugLevel);
 Logger.setLogLevel(Environment.logLevel);
 
 export {
-    Table
+    Table,
+    TableLite
 };
