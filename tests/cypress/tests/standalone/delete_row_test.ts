96a6979cb170e812dfd3e653a062d14c7308f09e
Valentijn Nieman <valentijnnieman@gmail.com>
2018-10-23 14:57:24 -0400

    Refactor standalone/delete_row_test to sort twice

--- tests/cypress/tests/standalone/delete_row_test.ts
+++ tests/cypress/tests/standalone/delete_row_test.ts
@@ -10,9 +10,9 @@ describe('delete', () => {
     });
 
     it('can delete row when sorted', () => {
-        cy.get('tr th.column-0 .sort').last().click({ force: true });
-        DashTable.getCell(0, 0).within(() => cy.get('.dash-cell-value').should('have.html', '1'));
+        cy.get('tr th.column-0 .sort').last().click({ force: true }).click({ force: true });
+        DashTable.getCell(0, 0).within(() => cy.get('.dash-cell-value').should('have.html', '4999'));
         DashTable.getDelete(0).click();
-        DashTable.getCell(0, 0).within(() => cy.get('.dash-cell-value').should('have.html', '2'));
+        DashTable.getCell(0, 0).within(() => cy.get('.dash-cell-value').should('have.html', '4998'));
     });
 });
