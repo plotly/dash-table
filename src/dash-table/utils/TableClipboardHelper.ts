2b4a3e3873a18d32a23039cfb67e1953b3561bfe
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2018-10-26 15:02:15 -0400

    - fix copy/paste more rows than data size with overflow - unit tests

--- src/dash-table/utils/TableClipboardHelper.ts
+++ src/dash-table/utils/TableClipboardHelper.ts
@@ -4,8 +4,8 @@ import SheetClip from 'sheetclip';
 import Clipboard from 'core/Clipboard';
 import Logger from 'core/Logger';
 
-import { ActiveCell, Columns, Data, SelectedCells, ColumnType } from 'dash-table/components/Table/props';
-import isEditable from 'dash-table/derived/cell/isEditable';
+import { ActiveCell, Columns, Data, SelectedCells } from 'dash-table/components/Table/props';
+import applyClipboardToData from './applyClipboardToData';
 
 export default class TableClipboardHelper {
     public static toClipboard(e: any, selectedCells: SelectedCells, columns: Columns, data: Data) {
@@ -36,83 +36,23 @@ export default class TableClipboardHelper {
         overflowColumns: boolean = true,
         overflowRows: boolean = true
     ): { data: Data, columns: Columns } | void {
-        const text = Clipboard.get(ev);
+        const text = Clipboard.get(ev as ClipboardEvent);
         Logger.trace('TableClipboard -- get clipboard data: ', text);
 
         if (!text) {
             return;
         }
 
-        if (!overflowRows) {
-            Logger.debug(`Clipboard -- Sorting or filtering active, do not create new rows`);
-        }
-
-        if (!overflowColumns) {
-            Logger.debug(`Clipboard -- Do not create new columns`);
-        }
-
         const values = SheetClip.prototype.parse(text);
 
-        let newData = data;
-        const newColumns = columns;
-
-        if (overflowColumns && values[0].length + activeCell[1] >= columns.length) {
-            for (
-                let i = columns.length;
-                i < values[0].length + activeCell[1];
-                i++
-            ) {
-                newColumns.push({
-                    id: `Column ${i + 1}`,
-                    name: `Column ${i + 1}`,
-                    type: ColumnType.Text
-                });
-                newData.forEach(row => (row[`Column ${i}`] = ''));
-            }
-        }
-
-        const realActiveRow = derived_viewport_indices[activeCell[0]];
-        if (overflowRows && values.length + realActiveRow >= data.length) {
-            const emptyRow: any = {};
-            columns.forEach(c => (emptyRow[c.id] = ''));
-            newData = R.concat(
-                newData,
-                R.repeat(
-                    emptyRow,
-                    values.length + realActiveRow - data.length
-                )
-            );
-        }
-
-        const lastEntry = derived_viewport_indices.slice(-1)[0] || -1;
-        const viewportSize = derived_viewport_indices.length;
-
-        values.forEach((row: string[], i: number) =>
-            row.forEach((cell: string, j: number) => {
-                const viewportIndex = activeCell[0] + i;
-
-                let iRealCell: number | undefined = viewportSize > viewportIndex ?
-                    derived_viewport_indices[viewportIndex] :
-                    overflowRows ?
-                        lastEntry + (viewportIndex - viewportSize + 1) :
-                        undefined;
-
-                if (iRealCell === undefined) {
-                    return;
-                }
-
-                const jOffset = activeCell[1] + j;
-                const col = newColumns[jOffset];
-                if (col && isEditable(true, col)) {
-                    newData = R.set(
-                        R.lensPath([iRealCell, col.id]),
-                        cell,
-                        newData
-                    );
-                }
-            })
+        return applyClipboardToData(
+            values,
+            activeCell,
+            derived_viewport_indices,
+            columns,
+            data,
+            overflowColumns,
+            overflowRows
         );
-
-        return { data: newData, columns: newColumns };
     }
 }
