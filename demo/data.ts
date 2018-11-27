7a454dee6748dfa179c819328fc40bae0c3af3cd
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2018-11-27 10:01:39 -0500

    Refactor demo app

--- demo/data.ts
+++ demo/data.ts
@@ -1,42 +1,47 @@
 /* eslint no-magic-numbers: 0 */
 import * as R from 'ramda';
 
-const N_DATA = 5000;
+const N_DATA = 100000;
 
-export const mockData = unpackIntoColumnsAndData([
+export interface IDataMock {
+    columns: any[];
+    data: any[];
+}
+
+export const generateMockData = (rows: number) => unpackIntoColumnsAndData([
     {
         id: 'rows',
         type: 'numeric',
         editable: false,
-        data: gendata(i => i)
+        data: gendata(i => i, rows)
     },
 
     {
         id: 'ccc',
         name: ['City', 'Canada', 'Toronto'],
         type: 'numeric',
-        data: gendata(i => i),
+        data: gendata(i => i, rows)
     },
 
     {
         id: 'ddd',
         name: ['City', 'Canada', 'Montréal'],
         type: 'numeric',
-        data: gendata(i => i * 100),
+        data: gendata(i => i * 100, rows)
     },
 
     {
         id: 'eee',
         name: ['City', 'America', 'New York City'],
         type: 'numeric',
-        data: gendata(i => i),
+        data: gendata(i => i, rows)
     },
 
     {
         id: 'fff',
         name: ['City', 'America', 'Boston'],
         type: 'numeric',
-        data: gendata(i => i + 1)
+        data: gendata(i => i + 1, rows)
     },
 
     {
@@ -44,7 +49,7 @@ export const mockData = unpackIntoColumnsAndData([
         name: ['City', 'France', 'Paris'],
         type: 'numeric',
         editable: true,
-        data: gendata(i => i * 10),
+        data: gendata(i => i * 10, rows)
     },
 
     {
@@ -53,7 +58,8 @@ export const mockData = unpackIntoColumnsAndData([
         type: 'dropdown',
         clearable: true,
         data: gendata(
-            i => ['Humid', 'Wet', 'Snowy', 'Tropical Beaches'][i % 4]
+            i => ['Humid', 'Wet', 'Snowy', 'Tropical Beaches'][i % 4],
+            rows
         )
     },
 
@@ -63,7 +69,8 @@ export const mockData = unpackIntoColumnsAndData([
         type: 'dropdown',
         editable: false,
         data: gendata(
-            i => ['Humid', 'Wet', 'Snowy', 'Tropical Beaches'][i % 4]
+            i => ['Humid', 'Wet', 'Snowy', 'Tropical Beaches'][i % 4],
+            rows
         )
     },
 
@@ -71,7 +78,7 @@ export const mockData = unpackIntoColumnsAndData([
         id: 'aaa',
         name: ['', 'Weather', 'Temperature'],
         type: 'numeric',
-        data: gendata(i => i + 1),
+        data: gendata(i => i + 1, rows)
     },
 
     {
@@ -79,16 +86,16 @@ export const mockData = unpackIntoColumnsAndData([
         name: ['', 'Weather', 'Temperature-RO'],
         type: 'numeric',
         editable: false,
-        data: gendata(i => i + 1),
+        data: gendata(i => i + 1, rows)
     }
 ]);
 
-export const mockDataSimple = unpackIntoColumnsAndData([
+export const mockDataSimple = (rows: number) => unpackIntoColumnsAndData([
     {
         id: 'aaa',
         name: 'Temperature',
         type: 'numeric',
-        data: gendata(i => i + 1),
+        data: gendata(i => i + 1, rows)
     },
 
     {
@@ -97,13 +104,14 @@ export const mockDataSimple = unpackIntoColumnsAndData([
         type: 'numeric',
         options: ['Humid', 'Wet', 'Snowy', 'Tropical Beaches'].map(i => ({
             label: i,
-            value: i,
+            value: i
         })),
         clearable: true,
         data: gendata(
-            i => ['Humid', 'Wet', 'Snowy', 'Tropical Beaches'][i % 4]
-        ),
-    },
+            i => ['Humid', 'Wet', 'Snowy', 'Tropical Beaches'][i % 4],
+            rows
+        )
+    }
 ]);
 
 export const miniData = unpackIntoColumnsAndData([
@@ -112,23 +120,24 @@ export const miniData = unpackIntoColumnsAndData([
     },
     {
         id: 'bbb', name: 'tomato', data: [3, 2, 1]
-    },
+    }
 ]);
 
-function unpackIntoColumnsAndData(columns) {
-    const mockData = { columns: [], data: []};
+function unpackIntoColumnsAndData(columns: any[]): IDataMock {
+    const mocked: any = { columns: [], data: [] };
+
     columns.forEach(col => {
-        col.data.forEach((v, i) => {
-            if (!mockData.data[i]) {
-                mockData.data[i] = {};
+        col.data.forEach((v: any, i: number) => {
+            if (!mocked.data[i]) {
+                mocked.data[i] = {};
             }
-            mockData.data[i][col.id] = v;
+            mocked.data[i][col.id] = v;
         });
-        mockData.columns.push(R.dissoc('data', col));
+        mocked.columns.push(R.dissoc('data', col));
     });
-    return mockData;
+    return mocked;
 }
 
-function gendata(func, ndata = N_DATA) {
+function gendata(func: (i: number) => any, ndata = N_DATA) {
     return R.range(1, ndata).map(func);
 }
