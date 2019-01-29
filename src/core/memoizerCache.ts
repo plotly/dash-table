542654560e3b711e50842aa39ef7028a27f6831f
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2019-01-28 20:17:46 -0500

    - new memoizerCache -- beter type inteference - refactor dropdowns and wrappers calculation

--- src/core/memoizerCache.ts
+++ src/core/memoizerCache.ts
@@ -2,17 +2,13 @@ import * as R from 'ramda';
 import { memoizeOne } from 'core/memoizer';
 
 type CacheKeyFragment = string | number | boolean;
-
-function memoize<TArgs extends any[], TEntry>(fn: (...args: TArgs) => TEntry) {
-    return memoizeOne((...args: TArgs) => fn(...args));
-}
+type CacheKey = CacheKeyFragment[];
 
 export default function memoizerCache<
     TKey extends CacheKeyFragment[],
     TArgs extends any[],
     TEntry
-    >(fn: (...args: TArgs) => TEntry)
-{
+>(fn: (...args: TArgs) => TEntry) {
     const cache = new Map<CacheKeyFragment, any>();
 
     return (key: TKey, ...args: TArgs): TEntry => {
@@ -25,7 +21,34 @@ export default function memoizerCache<
 
         return (
             fnCacne.get(lastKey) ||
-            fnCacne.set(lastKey, memoize<TArgs, TEntry>(fn)).get(lastKey)
+            fnCacne.set(lastKey, memoizeOne(fn)).get(lastKey)
         )(...args);
     };
 }
+
+export const memoizerCache2 = <TKey extends CacheKey>() => {
+    return <TEntryFn extends (...a: any[]) => any>(fn: TEntryFn) => {
+        const cache = new Map<CacheKeyFragment, any>();
+
+        function getCache(...key: TKey) {
+            const cacheKeys = key.slice(0, -1);
+
+            return R.reduce((c, fragment) => {
+                return c.get(fragment) || c.set(fragment, new Map()).get(fragment);
+            }, cache, cacheKeys) as Map<CacheKeyFragment, any>;
+        }
+
+        function get(...key: TKey) {
+            const lastKey = key.slice(-1)[0];
+
+            const nestedCache = getCache(...key);
+
+            return (
+                nestedCache.get(lastKey) ||
+                nestedCache.set(lastKey, memoizeOne(fn)).get(lastKey)
+            ) as TEntryFn;
+        }
+
+        return { get };
+    };
+};
