14e813f64e45a587eca73127cba9b86cfede07f8
Ryan Patrick Kyle <ryan@plot.ly>
2019-06-21 15:38:14 -0400

    fix up internal.R

--- R/internal.R
+++ R/internal.R
@@ -1,15 +1,12 @@
-.dashTable_js_metadata <- function() {
-deps_metadata <- list(`dash_table_0` = structure(list(name = "dash_table_0",
-version = "0", src = list(href = NULL,
-file = "deps"), meta = NULL,
-script = 'bundle.js',
-stylesheet = NULL, head = NULL, attachment = NULL, package = "dashTable",
-all_files = FALSE), class = "html_dependency"),
-`dash_table_1` = structure(list(name = "dash_table_1",
-version = "1", src = list(href = NULL,
-file = "deps"), meta = NULL,
-script = 'bundle.js.map',
-stylesheet = NULL, head = NULL, attachment = NULL, package = "dashTable",
+.dashTable_js_metadata <- function () {
+deps_metadata <- list(dash_table = structure(list(name = "dash_table", 
+version = "0", src = list(href = NULL, file = "deps"), 
+meta = NULL, script = "bundle.js", stylesheet = NULL, 
+head = NULL, attachment = NULL, package = "dashTable", 
+all_files = FALSE), class = "html_dependency"), dash_table = structure(list(name = "dash_table", 
+version = "0", src = list(href = NULL, file = "deps"), 
+meta = NULL, script = "bundle.js.map", stylesheet = NULL, 
+head = NULL, attachment = NULL, package = "dashTable", 
 all_files = FALSE), class = "html_dependency"))
 return(deps_metadata)
 }
