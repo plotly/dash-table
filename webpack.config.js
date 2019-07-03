9a4d7fbeda77aaf164623e747d5d85b91a4d6ec7
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2019-07-03 09:19:13 -0400

    externalize prop-types correctly in prod build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 let config = require('./.config/webpack/base.js')();
-config.externals['prop-types'] = 'PropTypes';
+config.externals['prop-types'] = {
+    root: 'PropTypes',
+    commonjs2: 'prop-types',
+    commonjs: 'prop-types',
+    amd: 'prop-types'
+};
 
 module.exports = config;
