f4b3b1455ce3fe683ddecf7a510ec21717f8b1f8
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2019-07-04 16:23:55 -0400

    fix lint

--- src/dash-table/dash/DataTable.js
+++ src/dash-table/dash/DataTable.js
@@ -12,11 +12,11 @@ import Sanitizer from './Sanitizer';
 import MenuBar from 'dash-table/components/MenuBar';
 
 /**
- * Dash DataTable is an interactive table component designed for 
+ * Dash DataTable is an interactive table component designed for
  * designed for viewing, editing, and exploring large datasets.
  * DataTable is rendered with standard, semantic HTML <table/> markup,
  * which makes it accessible, responsive, and easy to style. This
- * component was written from scratch in React.js specifically for the 
+ * component was written from scratch in React.js specifically for the
  * Dash community. Its API was designed to be ergonomic and its behavior
  * is completely customizable through its properties.
  */
