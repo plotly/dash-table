4c39f5925acce214351955882ccd104b77254a5a
byron <byron@plot.ly>
2019-07-09 21:59:03 -0400

    update test case for download

--- tests/integration/test_table_export.py
+++ tests/integration/test_table_export.py
@@ -1,20 +1,26 @@
+import os
 import pandas as pd
-import dash
 import dash_table
-
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
+import dash
+import dash.testing.wait as wait
 
 
 def test_tbex001_table_export(dash_duo):
+    df = pd.read_csv(
+        "https://raw.githubusercontent.com/plotly/datasets/master/solar.csv"
+    )
     app = dash.Dash(__name__)
     app.layout = dash_table.DataTable(
-        id='table',
+        id="table",
         columns=[{"name": i, "id": i} for i in df.columns],
-        data=df.to_dict('records'),
-        export_format='xlsx'
+        data=df.to_dict("records"),
+        export_format="xlsx",
     )
     dash_duo.start_server(app)
     dash_duo.find_element(".export").click()
 
-    import time
-    time.sleep(600)
+    download = os.path.sep.join((dash_duo.download_path, "Data.xlsx"))
+    wait.until(lambda: os.path.exists(download), timeout=2)
+
+    df_bis = pd.read_excel(download)
+    assert set(df_bis.columns) == set(df.columns)
