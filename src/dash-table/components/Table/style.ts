0948ccafc83b0148a3d812448caff70866bdb192
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2019-07-18 12:52:23 -0400

    lint

--- src/dash-table/components/Table/style.ts
+++ src/dash-table/components/Table/style.ts
@@ -1,6 +1,6 @@
 import { config, library, dom } from '@fortawesome/fontawesome-svg-core';
 import { faEyeSlash, faTrashAlt } from '@fortawesome/free-regular-svg-icons';
-import { faEraser, faPencilAlt, faSort, faSortDown, faSortUp } from '@fortawesome/free-solid-svg-icons'
+import { faEraser, faPencilAlt, faSort, faSortDown, faSortUp } from '@fortawesome/free-solid-svg-icons';
 
 config.searchPseudoElements = true;
 
