24514f8cbc69f58cce5f001da2469ed03a4a9905
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2019-08-23 14:40:55 -0400

    update storybook config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,9 +1,13 @@
 let babel = require('./babel.config.js');
 let config = require('./../.config/webpack/base.js')({
-    babel
+    babel,
+    preprocessor: {
+        variables: {
+            mode: 'eager'
+        }
+    }
 });
 
 config.externals = {};
-delete config.plugins;
 
 module.exports = config;
