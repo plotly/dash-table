f97cd9be2be12d0571a6e372122df8a6d98a4aaf
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-10-01 12:25:00 -0400

    Update src/dash-table/dash/DataTable.js
    Co-Authored-By: Marc-André Rivet <Marc-Andre-Rivet@users.noreply.github.com>

--- src/dash-table/dash/DataTable.js
+++ src/dash-table/dash/DataTable.js
@@ -732,7 +732,7 @@ export const propTypes = {
     page_current: PropTypes.number,
 
     /**
-     * `page_count` represents the number of the last page of the
+     * `page_count` represents the number of the pages in the
      * paginated table. This is really only useful when performing
      * backend pagination, since the front end is able to use the
      * full size of the table to calculate the number of pages.
