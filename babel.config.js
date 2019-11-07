c7fed230345d7b2ecfe650d4c3a05339dc5fd7af
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2019-11-07 16:28:11 -0500

    - es5 check on artifacts - use preset-env

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,5 @@
 const presets = [
-    ['@babel/env', {
-        targets: {
-            browsers: [
-                'last 2 Chrome versions',
-                'last 2 Firefox versions',
-                'last 2 Safari versions',
-                'last 2 Edge versions',
-                'Explorer 11'
-            ]
-        },
-        useBuiltIns: 'usage',
-        corejs: 3
-    }],
+    '@babel/preset-env',
     '@babel/preset-react'
 ];
 
