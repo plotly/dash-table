3a2d8de670e57f0455571a31a6d5fc2b70acf882
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-12-12 13:05:15 -0500

    Change getCell to getCellById.

--- tests/cypress/tests/standalone/markdown_test.ts
+++ tests/cypress/tests/standalone/markdown_test.ts
@@ -18,12 +18,12 @@ describe('markdown cells', () => {
             // rows 1, 10, 100, 1000 all have 4 pound symbols
             // row 1001 has 5 pound symbols and row 1002 has 0 pound symbols;
             // first cell should have displayed value "1002"
-            DashTable.getCell(0, 0).within(() => cy.get('.dash-cell-value > p').should('have.html', 'row 1002'));
+            DashTable.getCellById(0, 'markdown-headers').within(() => cy.get('.dash-cell-value > p').should('have.html', 'row 1002'));
 
             cy.get('tr th.column-0 .column-header--sort').click();
             // rows 999, 998, 997, 996 have, respectively, 3, 2, 1, and 0 pound symbols;
             // first cell should have displayed value "##### row 995"
-            DashTable.getCell(0, 0).within(() => cy.get('.dash-cell-value > h5').should('have.html', 'row 995'));
+            DashTable.getCellById(0, 'markdown-headers').within(() => cy.get('.dash-cell-value > h5').should('have.html', 'row 995'));
         });
 
         it('emphasized text', () => {
@@ -33,11 +33,11 @@ describe('markdown cells', () => {
             cy.get('tr th.column-1 .column-header--sort').last().click();
 
             // "*" < "_"; first cell should start with "*", i.e., be in an odd-numbered row
-            DashTable.getCell(0, 1).within(() => cy.get('.dash-cell-value > p > em').should('have.html', '1'));
+            DashTable.getCellById(0, 'markdown-italics').within(() => cy.get('.dash-cell-value > p > em').should('have.html', '1'));
 
             cy.get('tr th.column-1 .column-header--sort').last().click();
             // first cell should start with "_", i.e., be in an even-numbered row
-            DashTable.getCell(0, 1).within(() => cy.get('.dash-cell-value > p > em').should('have.html', '998'));
+            DashTable.getCellById(0, 'markdown-italics').within(() => cy.get('.dash-cell-value > p > em').should('have.html', '998'));
         });
 
         it('links', () => {
@@ -47,13 +47,13 @@ describe('markdown cells', () => {
             cy.get('tr th.column-2 .column-header--sort').last().click();
 
             // "]" > "0"; first cell should have row number 1000
-            DashTable.getCell(0, 2).within(() => cy.get('.dash-cell-value > p > a').should('have.html', 'Learn about 1000'));
-            DashTable.getCell(0, 2).within(() => cy.get('.dash-cell-value > p > a').should('have.attr', 'href', 'http://en.wikipedia.org/wiki/1000'));
+            DashTable.getCellById(0, 'markdown-links').within(() => cy.get('.dash-cell-value > p > a').should('have.html', 'Learn about 1000'));
+            DashTable.getCellById(0, 'markdown-links').within(() => cy.get('.dash-cell-value > p > a').should('have.attr', 'href', 'http://en.wikipedia.org/wiki/1000'));
 
             // "]" > "9"; first cell should have row number 9
             cy.get('tr th.column-2 .column-header--sort').last().click();
-            DashTable.getCell(0, 2).within(() => cy.get('.dash-cell-value > p > a').should('have.html', 'Learn about 9'));
-            DashTable.getCell(0, 2).within(() => cy.get('.dash-cell-value > p > a').should('have.attr', 'href', 'http://en.wikipedia.org/wiki/9'));
+            DashTable.getCellById(0, 'markdown-links').within(() => cy.get('.dash-cell-value > p > a').should('have.html', 'Learn about 9'));
+            DashTable.getCellById(0, 'markdown-links').within(() => cy.get('.dash-cell-value > p > a').should('have.attr', 'href', 'http://en.wikipedia.org/wiki/9'));
         });
 
         it('images', () => {
@@ -61,10 +61,10 @@ describe('markdown cells', () => {
             cy.get('tr th.column-8 .column-header--sort').last().click();
             cy.get('tr th.column-8 .column-header--sort').last().click();
 
-            DashTable.getCell(0, 8).within(() => cy.get('.dash-cell-value > p > img').should('have.attr', 'alt', 'image 1 alt text'));
+            DashTable.getCellById(0, 'markdown-images').within(() => cy.get('.dash-cell-value > p > img').should('have.attr', 'alt', 'image 1 alt text'));
 
             cy.get('tr th.column-8 .column-header--sort').last().click();
-            DashTable.getCell(0, 8).within(() => cy.get('.dash-cell-value > p > img').should('have.attr', 'alt', 'image 999 alt text'))
+            DashTable.getCellById(0, 'markdown-images').within(() => cy.get('.dash-cell-value > p > img').should('have.attr', 'alt', 'image 999 alt text'))
         });
 
         it('tables', () => {
@@ -77,10 +77,10 @@ describe('markdown cells', () => {
             cy.get('tr th.column-4 .column-header--sort').last().click();
             cy.get('tr th.column-4 .column-header--sort').last().click();
 
-            DashTable.getCell(0, 4).within(() => cy.get('.dash-cell-value > table > tbody > tr > td').first().should('have.html', '1'));
+            DashTable.getCellById(0, 'markdown-tables').within(() => cy.get('.dash-cell-value > table > tbody > tr > td').first().should('have.html', '1'));
 
             cy.get('tr th.column-4 .column-header--sort').last().click();
-            DashTable.getCell(0, 4).within(() => cy.get('.dash-cell-value > table > tbody > tr > td').first().should('have.html', '999'));
+            DashTable.getCellById(0, 'markdown-tables').within(() => cy.get('.dash-cell-value > table > tbody > tr > td').first().should('have.html', '999'));
         });
     });
 
@@ -146,7 +146,7 @@ describe('markdown cells', () => {
         it('correctly redirects', () => {
             cy.visit(`http://localhost:8080?mode=${AppMode.Markdown}`);
             // change href, since Cypress raises error when navigating away from localhost
-            DashTable.getCell(10, 2).within(() => cy.get('.dash-cell-value > p > a').invoke('attr', 'href', '#testlinkclick').click().click());
+            DashTable.getCellById(10, 'markdown-links').within(() => cy.get('.dash-cell-value > p > a').invoke('attr', 'href', '#testlinkclick').click().click());
             cy.url().should('include', `#testlinkclick`);
         });
     });
