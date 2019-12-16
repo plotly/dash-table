8ff279ba0ec3c5c659baa9d43e4d4f9ea9327c01
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-12-16 14:08:11 -0500

    Add test for using window.hljs if defined.

--- tests/cypress/tests/standalone/markdown_test.ts
+++ tests/cypress/tests/standalone/markdown_test.ts
@@ -147,13 +147,22 @@ describe('markdown cells', () => {
     });
 
     describe('loading highlightjs', () => {
-        beforeEach(() => {
+        it('loads highlight.js and does not attach hljs to window', () => {
             cy.visit(`http://localhost:8080?mode=${AppMode.Markdown}`);
             // wait for highlight.js to highlight code
             DashTable.getCellById(0, 'markdown-code-blocks').within(() => cy.get('code.language-python span.hljs-title'));
-        });
-        it('does not attach hljs to window', () => {
             cy.window().should('not.have.property', 'hljs');
+        });
+        it('uses window.hljs if defined', () => {
+            // define custom hljs object that always returns 'hljs override', and attach it to the window
+            cy.on('window:before:load', win => {
+                win.hljs = {
+                    getLanguage: (lang) => false, // force auto-highlight
+                    highlightAuto: (str) => { return { value: 'hljs override' } }
+                };
+            });
+            cy.visit(`http://localhost:8080?mode=${AppMode.Markdown}`);
+            DashTable.getCellById(0, 'markdown-code-blocks').within(() => cy.get('code.language-python').should('have.html', 'hljs override'));
         });
     });
 });
