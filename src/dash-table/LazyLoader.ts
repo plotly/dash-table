ede6b5edb67f418c6de4dcb1fba0568d76c4c89e
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-12-17 15:41:31 -0500

    Move third-party highlight.js file.

--- src/dash-table/LazyLoader.ts
+++ src/dash-table/LazyLoader.ts
@@ -4,7 +4,7 @@ export default class LazyLoader {
     }
 
     public static get hljs() {
-        return Promise.resolve(window.hljs || import(/* webpackChunkName: "highlight", webpackMode: "$${{mode}}" */ '../../third-party/highlight.js').then(result => result.default));
+        return Promise.resolve(window.hljs || import(/* webpackChunkName: "highlight", webpackMode: "$${{mode}}" */ '../third-party/highlight.js').then(result => result.default));
     }
 
     public static table() {
