66b59a2ae4f2c934f17d4798e2ad10de9f101af4
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2020-04-08 16:02:24 -0400

    - undo fix - new test (will fail, to pass after the fix added back)

--- src/dash-table/components/CellDropdown/index.tsx
+++ src/dash-table/components/CellDropdown/index.tsx
@@ -43,7 +43,6 @@ export default class CellDropdown extends PureComponent<IProps> {
                 onChange={(newValue: any) => {
                     onChange(newValue ? newValue.value : newValue);
                 }}
-                scrollMenuIntoView={false}
                 onOpen={this.handleOpenDropdown}
                 options={dropdown}
                 placeholder={''}
