55658258a7943a318065be46e32044a354d6f244
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2020-11-05 17:10:57 -0500

    percy

--- tests/selenium/conftest.py
+++ tests/selenium/conftest.py
@@ -376,9 +376,6 @@ def test(request, dash_thread_server, tmpdir):
         remote_url=request.config.getoption("remote_url"),
         headless=request.config.getoption("headless"),
         options=request.config.hook.pytest_setup_options(),
-        download_path=tmpdir.mkdir("dt-download").strpath,
-        percy_assets_root=request.config.getoption("percy_assets"),
-        percy_finalize=request.config.getoption("nopercyfinalize"),
-        pause=request.config.getoption("pause"),
+        download_path=tmpdir.mkdir("dt-download").strpath
     ) as dc:
         yield dc
