6d4b5c9de23b11675a8e66fb6a12f2eec5065d1f
Alexandr Romanenko <waralex@gmail.com>
2021-10-05 11:07:57 +0300

    Empty Julia Package

---
+++ src/DashTable.jl
@@ -0,0 +1,5 @@
+module DashTable
+    function __init__()
+        @warn "`using DashTable` is deprecated, all DashTable functions have been moved to Dash package. Use `using Dash` instead"
+    end
+end
